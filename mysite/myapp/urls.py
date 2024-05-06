e72fac78fb4c65fb4e64f42442de45bc967f033a
Saifcodex <saifahmed3693@gmail.com>
2024-05-06 10:18:42 +0600

    url added ******

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -38,10 +38,10 @@ urlpatterns = [
     path('checkout1/', views.checkout1, name='checkout1'),
 
     # doctor appointment urls
-    path('doctors/', views.doctors, name='doctors'),
+    path('SpecialBooks/', views.doctors, name='doctors'),
     path('doctor_search/', views.doctor_search, name='doctor_search'),
-    path('create_appointment/<int:doctor_id>/', views.create_appointment, name='create_appointment'),
-    path('cancel_appointment/<int:appointment_id>/<int:doctor_id>/', views.cancel_appointment, name='cancel_appointment'),
+    path('create_booking/<int:doctor_id>/', views.create_appointment, name='create_appointment'),
+    path('cancel_booking/<int:appointment_id>/<int:doctor_id>/', views.cancel_appointment, name='cancel_appointment'),
 
     #ICU Bookig
 
